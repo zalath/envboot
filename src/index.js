5d1ff64aa814341c0b7dca9c4159ff91a3c7aa83
zalath <zalath@163.com>
2019-08-09 09:22:56 +0800

    优化界面效果

---
+++ src/index.js
@@ -0,0 +1,101 @@
+const { app, BrowserWindow } = require('electron');
+const ipc = require('electron').ipcMain;
+const exec = require('child_process').exec;
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+const createWindow = () => {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 2000,
+    height: 1500,
+    frame:false,
+    webPreferences:{
+      nodeIntegration:true
+    }
+  });
+
+  mainWindow.webContents.once('dom-ready',()=>{
+    getconf(mainWindow);
+  });
+  
+  
+  mainWindow.setMenu(null);
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    // mainWindow = null;
+  });
+};
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
+ipc.on('openproject',function(event,args){
+  exec('code '+args['path'],{});
+});
+ipc.on('closeapp',function(event,args){
+  app.quit();
+})
+
+function getconf(a){
+  var path = __dirname+'/../c.json';
+  var fs = require('fs');
+  fs.readFile(path,'utf8',function(err,data){
+    if(err) return  console.log(err);
+    else{
+      data = JSON.parse(data);
+      a.webContents.send('initd',data);
+    }
+  })
+}
+
+ipc.on('boot',function(event,args){
+  var path = __dirname+'/../c.json';
+  var fs = require('fs');
+  fs.readFile(path,'utf8',function(err,data){
+    if(err) return  console.log(err);
+    else{
+      data = JSON.parse(data);
+      exec('"'+data['boot']['navicat']+'"');
+      exec('"'+data['boot']['filezilla']+'"');
+      exec('"'+data['boot']['chrome']+'"');
+      exec('code');
+    }
+  })
+})
